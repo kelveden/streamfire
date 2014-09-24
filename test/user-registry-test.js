1c4d7660d9cad5dd321a4d69e9af07182b666f84
adutton <alistair.dutton@microsoft.com>
2014-09-24 10:24:41 +0100

    Added in ability to search in user registry.

--- test/user-registry-test.js
+++ test/user-registry-test.js
@@ -5,7 +5,7 @@ var chai = require('chai'),
     UserRegistry = require('../lib/user-registry'),
     when = require('when');
 
-describe("user registry", function () {
+describe("user registry", function (done) {
     it("will retrieve a user from campfire if it does not exist in the registry", function () {
         var campfire = {
                 getUser: sinon.stub().returns({
@@ -24,6 +24,45 @@ describe("user registry", function () {
             .then(function (user) {
                 expect(user.user_id).to.equal(1234);
                 expect(campfire.getUser.callCount).to.equal(1);
+                done();
+            });
+    });
+
+    it("can return all users with names starting with a specified string", function (done) {
+        var stub = sinon.stub();
+        stub.onCall(0).returns({
+            then: function (f) {
+                return when.resolve(f({ name: "user1", user_id: 1 }));
+            }
+        });
+
+        stub.onCall(1).returns({
+            then: function (f) {
+                return when.resolve(f({ name: "bollox", user_id: 2 }));
+            }
+        });
+
+        stub.returns({
+            then: function (f) {
+                return when.resolve(f({ name: "user2", user_id: 3 }));
+            }
+        });
+
+        var campfire = {
+                getUser: stub
+            },
+            userRegistry = new UserRegistry({ campfire: campfire });
+
+        userRegistry.getUser(1)
+            .then(function () { return userRegistry.getUser(2); })
+            .then(function () { return userRegistry.getUser(3); })
+            .then(function () {
+                var results = userRegistry.search("use");
+
+                expect(results).to.have.length(2);
+                expect(results[0].name).to.equal("user1");
+                expect(results[1].name).to.equal("user2");
+                done();
             });
     });
 });
